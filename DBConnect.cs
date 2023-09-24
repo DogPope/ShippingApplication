663867e7cb19d659308dae51a6fc50937de39334
DogPope <114190696+DogPope@users.noreply.github.com>
2023-09-24 21:24:54 +0100

    Update DBConnect.cs
    Cleaning, Tidying

--- DBConnect.cs
+++ DBConnect.cs
@@ -7,7 +7,6 @@ namespace ShippingApplication
     class DBConnect
     {
         // Contains two strings for use in connecting to the database.
-        public const String oradb = "Data Source = localhost/orcl; User Id = C##Daniel; Password = password"; // Works at home.
-        //public const String oradb = "Data Source = oracle/orcl; User Id = T00158237; Password = hkh9J7rgp!iw"; // Works in college.
+        public const String oradb = "Data Source = localhost/orcl; User Id = C##Daniel; Password = password";
     }
 }
